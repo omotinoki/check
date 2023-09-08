140cf47c4220671b67c083e3aa8b8398b9799029
omotinoki <kk.mr194527@gmail.com>
2023-09-08 22:23:48 +0900

    0908_new2commit

--- EnglshTown/src/Main.java
+++ EnglshTown/src/Main.java
@@ -1,8 +1,10 @@
+import java.awt.GridBagLayout;
 import java.awt.event.ActionListener;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
 
 public class Main extends JFrame implements ActionListener {
 	private GridBagLayout gb;
-	private 
+	private JButton button;
 }
