0e98a9bd547443dd3e5303af7194d8c1864a1445
omotinoki <kk.mr194527@gmail.com>
2023-09-08 22:41:43 +0900

    0908_test

--- EnglshTown/src/Main.java
+++ EnglshTown/src/Main.java
@@ -3,8 +3,10 @@ import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 
 public class Main extends JFrame implements ActionListener {
 	private GridBagLayout gb;
 	private JButton button;
+	private JLabel lab;
 }
