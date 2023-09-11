e10151f51e1af74718e87f2e50acba93bba2aa8b
omotinoki <kk.mr194527@gmail.com>
2023-09-11 23:05:39 +0900

    0911_test

--- EnglshTown/src/Main.java
+++ EnglshTown/src/Main.java
@@ -4,9 +4,11 @@ import java.awt.event.ActionListener;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JTextField;
 
 public class Main extends JFrame implements ActionListener {
 	private GridBagLayout gb;
 	private JButton button;
 	private JLabel lab;
+	private JTextField text;
 }
